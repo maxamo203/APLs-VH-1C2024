0b0b86f83c5bb57e23b6501d803ffccf9aed8398
Agustín Zabalgoitia <aguszabalgoitia@gmail.com>
2024-04-20 19:22:25 -0300

    Arreglados los problemas con inotifywait Todavía falta hacer que haga los backups y que detecte los patrones

--- APL1/Bash/Ejercicio4/Ejercicio4.sh
+++ APL1/Bash/Ejercicio4/Ejercicio4.sh
@@ -32,6 +32,10 @@ function validarParametros {
 	echo "Se debe utilizar el comando -s/--salida o -k/--kill"
 	exit 6
     fi
+    if [[ ! -z "$kill" && ( ! -z "$salida" || ! -z "$patron" ) ]]; then
+	echo "El parámetro -k/--kill solo puede ser usado junto con -d/--directorio"
+	exit 7
+    fi
 }
 
 #Si ya hay una instancia del programa ejecutandose en un directorio, sale informando al usuario.
@@ -66,18 +70,21 @@ function validarDirectorio {
 #incluyendo la carpeta Ejercicio4 si es el único proceso ejecutándose
 function limpiarTmp {
 
-	archivo=`find /tmp/Ejercicio4 -name "$$.ej4"`
-	rm $archivo
+	archivoEj4=`find /tmp/Ejercicio4 -name "$$.ej4"`
+	rm $archivoEj4
+	archivoNlog=`find /tmp/Ejercicio4 -name "$$.nlog"`
+	rm $archivoNlog
 
 	if [[ ! `ls -A /tmp/Ejercicio4` ]]; then
 		rmdir /tmp/Ejercicio4
 	fi
 
+	kill "$notifyPID" #Matamos al notifywait
 	exit 0
 }
 
 #Si ya hay un proceso en el directorio, lo mata
-#Si no lo hay, sale informando al usuario
+#Si no lo hay, informa al usuario
 function matarProceso {
 
 	ruta=`realpath $directorio`
@@ -88,7 +95,6 @@ function matarProceso {
 			if [[ $rutaObtenida = $ruta ]]; then
 				PID=`basename $archivo .ej4`
 				echo "Matando al proceso $PID del directorio $ruta"
-				#TODO: hacer que al matar el proceso, no se muestre ningún mensaje que no sea el de arriba por pantalla
 				kill $PID
 				exit 0
 			fi
@@ -106,6 +112,64 @@ function archivoCreado {
 
 function archivoModificado {
 	echo "[`date`]: El archivo $1$2 fue modificado" >> $salida/registro.log
+}
+
+function hacerBackup {
+
+	archivoSalida="$salida/$1.tar"
+
+	#Nota: tar se reusa cobardemente a crear un archivo vacío.
+	if [[ ! -s "$1" ]]; then
+		echo "No se puede hacer un backup del archivo $1 ya que está vacío (Esto tendría que estar en el registro)"
+		return 1
+	fi
+
+	#Si no hay un archivo de backup lo tenemos que crear
+	if [[ ! -e "$archivoSalida" ]]; then
+		tar -cf $archivoSalida --transform="s,^,`date +%Y%m%d-%H%M%S` ," $1
+	fi
+
+	gzip $archivoSalida
+}
+
+function observarDirectorio {
+
+	#El notify lo ejecuto en el fondo para que no haga lío
+	inotifywait -m -q -e create,modify "$directorio" >> /tmp/Ejercicio4/$$.nlog &
+	notifyPID="$!" #Guardo su PID para poder matarlo después
+	ultimaLinea=""
+	
+	while true; do
+		#Agarra la última linea del archivo donde inotifywait está guardando la salida
+		nuevaLinea=`tail -n 1 /tmp/Ejercicio4/$$.nlog`
+
+		#Esto funciona así, como es un dolor de huevos usar inotify wait de una en el script por múltiples razones
+		#lo que hice acá fue que el inotify guardara su output en un archivo temporal .nlog
+		#y que el script del ejercicio4 leyera la última línea que está ahí.
+		
+		#De momento, creo que tiene un error medio grave, si se modifica el archivo varias veces seguidas, no lo nota.
+		#Lo mismo si se crea el archivo varias veces seguidas
+		if [[ "$nuevaLinea" != "$ultimaLinea" ]]; then
+			ultimaLinea="$nuevaLinea"
+
+			evento=`echo $ultimaLinea | awk '{print $2}'`
+			archivoAfectado=`echo $ultimaLinea |awk '{print $3}'`
+
+			sed '$d' /tmp/Ejercicio4/$$.nlog > /tmp/Ejercicio4/$$.nlog
+
+			case "$evento" in
+				"CREATE")
+					archivoCreado $archivoAfectado
+				;;
+				"MODIFY")
+					archivoModificado $archivoAfectado
+				;;
+			esac
+
+			hacerBackup $archivoAfectado
+
+		fi
+	done
 }
 
 #Cuando le mandan alguna de estas señales ejecuta limpiarTmp
@@ -169,20 +233,12 @@ if [[ "$kill" = "true" ]]; then
 	matarProceso
 elif [[ "$shell" = "true" ]]; then
 	validarDirectorio
-
-	#ESTO NO FUNCIONA, CREO QUE ENCONTRÉ UNA MANERA. MAÑANA VEO DE ARREGLARLO
-	inotifywait -q -m -e modify,delete,create $directorio | while read DIRECTORY EVENT FILE; do
-	    case $EVENT in
-	        MODIFY* )
-	            file_modified "$DIRECTORY" "$FILE"
-		;;
-		CREATE* )
-	            file_created "$DIRECTORY" "$FILE"
-	        ;;
-	    esac
-	done
+	
+	notifyPID=""
+	observarDirectorio
+else
+	#Si no se le pasa el parámetro --shell, asume que se quiere ejecutar el script en el fondo.
+	#Para lograr esto, se ejecuta el mismo comando en una subshell
+	( ./Ejercicio4.sh -d "$directorio" -s "$salida" --shell &)
 fi
 
-#Si no se le pasa el parámetro --shell, asume que se quiere ejecutar el script en el fondo.
-#Para lograr esto, se ejecuta el mismo comando en una subshell
-( ./Ejercicio4.sh -d "$directorio" -s "$salida" --shell &)
