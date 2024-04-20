1033d5ea22d25e19a16e69051b02f882ba2a4214
maxamo203 <maxamo.203@gmail.com>
2024-04-20 19:18:57 -0300

    ej4 ps, manejo de subprocesos

--- APL1/PowerShell/Ejercicio4/Ejercicio4.ps1
+++ APL1/PowerShell/Ejercicio4/Ejercicio4.ps1
@@ -42,6 +42,11 @@ function dejarMonitorizar{
   #leer el PID
   #kill del PID
   #borrar el registro de esa linea
+  if (-not (Test-Path "/tmp/Ejercicio4/running.ej4")) {
+    Write-Error "No hay ningun directorio monitoreandose"
+    exit 4
+  }
+
   $coincidencias = Select-String -Path "/tmp/Ejercicio4/running.ej4" -Pattern $directorio #trae las lineas donde se indica el directorio (deberia ser solo una)
   if (-not $coincidencias){
     Write-Error "No se esta monitoreando $directorio, si quieres monitorearlo indica el parametro -salida y -patron"
@@ -54,13 +59,20 @@ function dejarMonitorizar{
 
   $lineaABorrar =  $coincidencias[0].ToString().Split()[0].Split(":")[1]
   $lineas = Get-Content "/tmp/Ejercicio4/running.ej4"
-  Clear-Content "/tmp/Ejercicio4/running.ej4" #borra todas las lineas para escribirlas devuelta, sin la que mate recien
-  for ($i = 0; $i -lt $lineas.Count; $i++) {
-    if ($i -eq $lineaABorrar - 1){
-      continue
-    }
-    Write-Output $lineas[$i] >> "/tmp/Ejercicio4/running.ej4"
+  if($lineas.Count -eq 1){ #si solo queda un proceso, borra el archivo directamente
+    Remove-Item -Path "/tmp/Ejercicio4" -Recurse
   }
+  else{
+
+    Clear-Content "/tmp/Ejercicio4/running.ej4" #borra todas las lineas para escribirlas devuelta, sin la que mate recien
+    for ($i = 0; $i -lt $lineas.Count; $i++) {
+      if ($i -eq $lineaABorrar - 1){
+        continue
+      }
+      Write-Output $lineas[$i] >> "/tmp/Ejercicio4/running.ej4"
+    }
+  }
+  Write-Output "Ya no se esta monitoreando el directorio $directorio"
 }
 
 $bloque = {
