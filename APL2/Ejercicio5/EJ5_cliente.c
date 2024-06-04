b5b1ea11cbd842a89231fef3889b0588e24c3a53
maxamo203 <maxamo.203@gmail.com>
2024-06-04 18:57:44 -0300

    ej5 terminado, falta probar

--- APL2/Ejercicio5/EJ5_cliente.c
+++ APL2/Ejercicio5/EJ5_cliente.c
@@ -1,21 +1,75 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "../Bibliotecas/headers/tablero.h"
+#include "../Bibliotecas/headers/parsearParametros.h"
 #include <errno.h>
 #include <termios.h>
 
+#define MAX_NAME_LENGTH 30
+void mostrarAyuda()
+{
+    puts("Mostrando AYUDA");
+    puts("-n/--nickname --> Indica el nombre con el que va a jugar el usuario");
+    puts("-p/--puerto --> Indica el puerto donde esta escuchando el servidor");
+    puts("-s/--servidor --> Indica la direccion (IP) del servidor");
+}
+int startsWith(const char* src, const char* obj){
+    while(*src != 0 && *obj != 0){
+        if(*src++ != *obj++){
+            return 0;
+        }
+    }
+    return *obj == 0;
+}
+int isNumber(const char *str)
+{
+    while (*str)
+    {
+        if (!isdigit(*str))
+        {
+            return 0; // No es un número
+        }
+        str++;
+    }
+    return 1; // Es un número
+}
+int validarParametros(char* nombre, char* puerto, char* servidor){
+    if(nombre == NULL || puerto == NULL || servidor == NULL){
+        puts("ERROR: El nickname, puerto e ip del servidor son requeridos");
+        return 0;
+    }
+    if(!(isNumber(puerto) && atoi(puerto)>=1024)){
+        puts("ERROR: El puerto debe ser un numero mayor a 1024");
+        return 0;
+    }
+    if(strlen(nombre) > MAX_NAME_LENGTH){
+        printf("ERROR: El nombre no puede tener mas de %d caracteres\n", MAX_NAME_LENGTH);
+        return 0;
+    }
+}
 int main(int argc, char* argv[]){
-    char nombre[30];
-    strcpy(nombre, argv[1]);
+    char *nombre = NULL, *puerto = NULL,*servidor, *help = NULL;
+    char *opcionesLargas[] = {"nickname","puerto","servidor", "help"};
+    char **variables[] = {&nombre, &puerto, &servidor, &help};
+    parsearParametros(argv, argc, "n:p:s:h", opcionesLargas, 4, variables);
+    if(help){
+        mostrarAyuda();
+        freeParametros(4, variables);
+        return 0;
+    }
+    if(validarParametros(nombre, puerto, servidor) == 0){
+        freeParametros(4, variables);
+        return 1;
+    }
     struct sockaddr_in socketConfig;
     Tablero tablero;
     int opt = 1;
     socketConfig.sin_family = AF_INET;
-    socketConfig.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &socketConfig.sin_addr);
+    socketConfig.sin_port = htons(atoi(puerto));
+    inet_pton(AF_INET, servidor, &socketConfig.sin_addr);
 
     int socketComunicacion = socket(AF_INET, SOCK_STREAM, 0);
     // Habilitar SO_KEEPALIVE
@@ -26,6 +80,7 @@ int main(int argc, char* argv[]){
     if (resultadoConexion < 0)
     {
         puts("Error en la conexión");
+        close(socketComunicacion);
         return EXIT_FAILURE;
     }
 
@@ -33,11 +88,12 @@ int main(int argc, char* argv[]){
     int bytesRecibidos = 0;
     int entrada[2];
     char bytesEscritos[10];
+    char termino = 0;
     puts("Esperando partida...");
     while ((bytesRecibidos = read(socketComunicacion, buffer, sizeof(buffer) - 1))>0)
     {   
         if(strcmp(buffer, "TU TURNO") == 0){
-            printf("Tu tunro wachin\n");
+            printf("Ingresa la posicion (x y): ");
             tcflush(STDIN_FILENO, TCIFLUSH);
             while(scanf("%d %d", &entrada[0],&entrada[1]) != 2){
                 puts("Entrada invalida");
@@ -53,18 +109,15 @@ int main(int argc, char* argv[]){
         else{
             printf("%s\n", buffer);
             if(strcmp(buffer, "JUEGO INICIADO") == 0){
-                write(socketComunicacion, nombre, sizeof(nombre)); //manda su nombre
+                write(socketComunicacion, nombre, MAX_NAME_LENGTH); //manda su nombre
+            }
+            else if(startsWith(buffer, "RESULTADOS:")){
+                termino = 1;
             }
         }
-        //printf("cant bytes [%d] [%s]\n", bytesRecibidos, buffer);
-        //buffer[bytesRecibidos] = 0;
-        
-       
-        //TODO: limpiar buffer por entradas anteriores
-        
     }
-    printf("%d %d\n", errno, bytesRecibidos);
-    if(bytesRecibidos == -1){
+    //printf("%d %d\n", errno, bytesRecibidos);
+    if(!termino){
         puts("Servidor Caido");
     }
     close(socketComunicacion);
--- APL2/Ejercicio5/EJ5_servidor.c
+++ APL2/Ejercicio5/EJ5_servidor.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "../Bibliotecas/headers/tablero.h"
+#include "../Bibliotecas/headers/parsearParametros.h"
 #include <pthread.h>
 #include <semaphore.h>
 #include <signal.h>
@@ -14,18 +16,19 @@ typedef struct
     int nro;
     int socket;
 } parametrosThreads;
-typedef struct{
+typedef struct
+{
     char nombre[30];
 } TNombre;
 pthread_t *tids;
 sem_t *semaforosTurnos;
 sem_t semaforoAvisados;
-int cantJugadores = 3;
+int cantJugadores;
 int jugadoresConectados = 0;
-char * estadoJugadores; // guarda los aciertos de cada jugador o -1 si se desconecta
-TNombre * nombresJugadores;
+char *estadoJugadores; // guarda los aciertos de cada jugador o -1 si se desconecta
+TNombre *nombresJugadores;
 int jugadorActual = 0;
-
+int server;
 pthread_barrier_t barreraInicio;
 pthread_mutex_t mutex;
 Tablero tablero;
@@ -35,6 +38,51 @@ char sendBuff[128]; // el sizeof del tablero me da 72, por las dudas le doy el d
 
 int levantarServer(int puerto);
 
+void mostrarAyuda()
+{
+    puts("Mostrando AYUDA");
+    puts("-p/--puerto --> Indica el puerto donde va a quedar corriendo el servidor");
+    puts("-j/--jugadores --> Indica la cantidad de jugadores que va a esperar para iniciar la partida");
+}
+void terminarServer()
+{
+   
+}
+int isNumber(const char *str)
+{
+    while (*str)
+    {
+        if (!isdigit(*str))
+        {
+            return 0; // No es un número
+        }
+        str++;
+    }
+    return 1; // Es un número
+}
+int validarParametros(char *puerto, char *usuarios)
+{
+    if (puerto == NULL || usuarios == NULL)
+    {
+        puts("ERROR: Parametros insuficientes, puerto y cantidad de jugadores son requeridos");
+        return 0;
+    }
+    if (!isNumber(puerto) || !isNumber(usuarios))
+    {
+        puts("ERROR: Tanto el puerto como el usuario deben ser numero enteros");
+        return 0;
+    }
+    if (atoi(puerto) <= 1024)
+    {
+        puts("ERROR: El puerto debe ser mayor a 1024");
+        return 0;
+    }
+    if (atoi(usuarios) <= 0)
+    {
+        puts("ERROR: Debe haber al menos un usuario");
+        return 0;
+    }
+}
 void enviarTableroActualizado()
 {
     pthread_t mitid = pthread_self();
@@ -49,8 +97,9 @@ void enviarTableroActualizado()
     }
     char bufferAviso[50];
     int result = recv(params[ind].socket, &bufferAviso, 1, MSG_PEEK | MSG_DONTWAIT);
-    if(result == 0){ //se desconecto uno
-        puts("AJJAAAA");
+    if (result == 0)
+    { // se desconecto uno
+        puts("Jugador desconectado");
         estadoJugadores[ind] = -1;
         jugadoresConectados--;
         sem_post(&semaforoAvisados);
@@ -63,28 +112,22 @@ void enviarTableroActualizado()
     puts("Imprimio tablero en otro");
     sem_post(&semaforoAvisados);
 }
-void enviarResultados(int socket){
+void enviarResultados(int socket)
+{
     char bufferSend[2000];
     char bufferNombre[30];
     strcat(bufferSend, "RESULTADOS:\n");
-    for(int i = 0;i<jugadoresConectados;i++){
+    for (int i = 0; i < jugadoresConectados; i++)
+    {
         strcat(bufferSend, nombresJugadores[i].nombre);
-        strcat(bufferSend,": ");
-        sprintf(bufferNombre, "%d", estadoJugadores[i]); // Convertir el entero a una cadena
+        strcat(bufferSend, ": ");
+        sprintf(bufferNombre, "%d\n", estadoJugadores[i]); // Convertir el entero a una cadena
         strcat(bufferSend, estadoJugadores[i] < 0 ? "0" : bufferNombre);
     }
-    write(socket,bufferSend,sizeof(bufferSend));
+    write(socket, bufferSend, sizeof(bufferSend));
 }
 void *enviarTexto(void *arg)
 {
-    // struct sigaction sa;
-    // sa.sa_handler = enviarTableroActualizado;
-    // sa.sa_flags = 0; // Ensure the signal interrupts the open call
-    // sigemptyset(&sa.sa_mask);
-    // if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-    //     perror("sigaction");
-    //     exit(EXIT_FAILURE);
-    // }
     signal(SIGUSR1, enviarTableroActualizado);
     int socketCom = ((parametrosThreads *)arg)->socket;
     int nro = ((parametrosThreads *)arg)->nro;
@@ -99,29 +142,27 @@ void *enviarTexto(void *arg)
     write(socketCom, sendBuff, strlen(sendBuff));
     int rc = pthread_barrier_wait(&barreraInicio);
     write(socketCom, "JUEGO INICIADO", sizeof("JUEGO INICIADO"));
-    read(socketCom, nombresJugadores[nro].nombre,30); //recibe el nombre
+    read(socketCom, nombresJugadores[nro].nombre, 30); // recibe el nombre
     while (jugadoresConectados > 0 && !tableroCompleto(&tablero))
     {
         printf("Principio %d\n", nro);
-        if (sem_wait(&semaforosTurnos[nro]) == -1)
+        sem_wait(&semaforosTurnos[nro]);
+        if (tableroCompleto(&tablero))
         {
-            puts("Alto ahi compa;ero");
-        }
-        if(tableroCompleto(&tablero)){
             sem_post(&semaforosTurnos[(nro + 1) % cantJugadores]);
             break;
         }
         printf("%d-----\n", errno);
-        if (estadoJugadores[nro]>=0)
+        if (estadoJugadores[nro] >= 0)
         { // aunque el cliente se haya desconectado el server mantiene su estructura
             for (int i = 0; i < 2; i++)
             {
                 jugadorActual = nro;
-                printf("Algo %d\n", i);
                 usleep(10000);
                 write(socketCom, "TU TURNO", sizeof("TU TURNO"));
                 usleep(10000);
-                if(read(socketCom, receiveBuffer, sizeof(receiveBuffer)) == 0){ //por si se desconecta el que esta jugando en este memomento
+                if (read(socketCom, receiveBuffer, sizeof(receiveBuffer)) == 0)
+                { // por si se desconecta el que esta jugando en este memomento
                     puts("Jugador desconectado");
                     pthread_mutex_lock(&mutex);
                     jugadoresConectados--;
@@ -132,11 +173,13 @@ void *enviarTexto(void *arg)
                 memcpy(userInput, receiveBuffer, sizeof(receiveBuffer));
                 printf("%d %d Ingreso el usuario\n", userInput[1], userInput[0]);
                 int res = darVueltaPosicion(&tablero, userInput[1], userInput[0]);
-                if(res == 0){ //dio vuelta la misma posicion de antes
+                if (res == 0)
+                { // dio vuelta la misma posicion de antes
                     i--;
                     continue;
                 }
-                else if(res == 2){//si acierta
+                else if (res == 2)
+                { // si acierta
                     estadoJugadores[nro]++;
                 }
                 memcpy(sendBuff, &tablero, sizeof(tablero));
@@ -146,52 +189,76 @@ void *enviarTexto(void *arg)
                 {
                     if (i == nro || estadoJugadores[i] == -1)
                         continue; // que no se mande una señal a si mismo o a uno que no esta jugando
-                    printf("se;al enviada a %ld, soy %ld\n", tids[i], pthread_self());
+                    printf("señal enviada a %ld, soy %ld\n", tids[i], pthread_self());
                     pthread_kill(tids[i], SIGUSR1);
                 }
-                for (int i = 0; i < jugadoresConectados - 1; i++)
+                for (int i = 0; i < jugadoresConectados - 1; i++) // espero que todos los hilos hayan mandado la señal
                 {
                     sem_wait(&semaforoAvisados);
                 }
-                puts("Hasta aca");
             }
         }
         sem_post(&semaforosTurnos[(nro + 1) % cantJugadores]);
     }
-    if(estadoJugadores[nro]>=0){
+    if (estadoJugadores[nro] >= 0)
+    {
         enviarResultados(socketCom);
     }
     close(socketCom);
 }
-// devuelve el FD del socket abierto
 
 int main(int argc, char *argv[])
 {
+    struct sigaction sa;
+    sa.sa_handler = terminarServer;
+    sa.sa_flags = 0; // Ensure the signal interrupts the open call
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
-    int server = levantarServer(8080);
+    char *puerto = NULL, *usuarios = NULL, *help = NULL;
+    char *opcionesLargas[] = {"puerto", "jugadores", "help"};
+    char **variables[] = {&puerto, &usuarios, &help};
+    parsearParametros(argv, argc, "p:j:h", opcionesLargas, 3, variables);
+    if (help)
+    {
+        mostrarAyuda();
+        return 0;
+    }
+    if (!validarParametros(puerto, usuarios))
+    {
+        return 1;
+    }
+    server = levantarServer(atoi(puerto));
+    cantJugadores = atoi(usuarios);
 
-    int i = 0;
     tids = malloc(cantJugadores * sizeof(pthread_t));
     params = malloc(cantJugadores * sizeof(parametrosThreads));
     semaforosTurnos = malloc(cantJugadores * sizeof(sem_t));
-    pthread_mutex_init(&mutex, NULL);
-    sem_init(&semaforoAvisados, 0, 0);
     estadoJugadores = malloc(cantJugadores);
     nombresJugadores = malloc(cantJugadores * (sizeof(TNombre)));
-    while(1){
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&semaforoAvisados, 0, 0);
+    char señal = 0;
+    int i = 0;
+    while (1)
+    {
         if (pthread_barrier_init(&barreraInicio, NULL, cantJugadores) != 0)
-            {
-                perror("pthread_barrier_init");
-                return EXIT_FAILURE;
-            }
+        {
+            perror("pthread_barrier_init");
+            return EXIT_FAILURE;
+        }
         generarTablero(&tablero);
-        for(int i = 0;i<4;i++){
-                    for (int j = 0; j < 4; j++)
-                    {
-                        printf("%c %d-%d\n",tablero.tablero[i][j],i,j);
-                    }     
-                }
-        printf("%li--\n", sizeof(tablero));
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                printf("%c %d-%d\n", tablero.tablero[i][j], i, j);
+            }
+        }
         while (i < cantJugadores)
         {
             if (sem_init(&semaforosTurnos[i], 0, (i == 0) ? 1 : 0) != 0)
@@ -199,28 +266,45 @@ int main(int argc, char *argv[])
                 perror("sem_init");
                 exit(EXIT_FAILURE);
             }
-            params[i].socket = accept(server, (struct sockaddr *)NULL, NULL);
+            if ((params[i].socket = accept(server, (struct sockaddr *)NULL, NULL)) == -1)
+            {
+                señal = 1;
+                break;
+            }
             params[i].nro = i;
             pthread_create(&tids[i], NULL, enviarTexto, (void *)&(params[i]));
             i++;
         }
-        for (int i = 0; i < cantJugadores; i++)
-        {
-            pthread_join(tids[i], NULL);
-        }
-        
+        if (!señal)
+            for (int i = 0; i < cantJugadores; i++)
+            {
+                if (pthread_join(tids[i], NULL) != 0)
+                { // hubo interrupcion
+                    señal = 1;
+                    break;
+                }
+            }
+
         puts("Partida terminada");
         pthread_barrier_destroy(&barreraInicio);
-        for(int i =0;i<cantJugadores;i++){
+        for (int i = 0; i < cantJugadores; i++)
+        {
             sem_destroy(&semaforosTurnos[i]);
+        }
+        if (señal)
+        {
+            break;
         }
         i = 0;
         jugadoresConectados = 0;
     }
-    
-
-
-
+    puts("Saliendo...");
+    freeParametros(3, variables);
+    free(nombresJugadores);
+    free(estadoJugadores);
+    free(semaforosTurnos);
+    free(params);
+    free(tids);
     close(server);
     return 0;
 }
@@ -235,7 +319,7 @@ int levantarServer(int puerto)
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    // Configurar el socket para permitir la reutilización de la dirección
+    // configura el socket para permitir la reutilización del puerto
     if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
     {
         perror("setsockopt");
